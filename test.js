817e415037935c3e06b74e435e4c55cefdd4eafb
Matt Fili <matthewjosephfili@gmail.com>
2015-07-21 10:09:12 -0500

    chai testing up and running

--- test.js
+++ test.js
@@ -2,8 +2,21 @@ var should = require("chai").should()
 
 describe('Tests', function() {
     it('Truthiness', function () {
-      true.should.equal(true);
-
+      	true.should.equal(true);
 
     });
+});
+
+describe('Array', function () {
+	describe('#map()', function () {
+		it('should keep the same length', function () {
+			var array = [1,2,3,4,5];
+
+			var arrayLength = array.map(function (item) {
+				return false;
+			}).length;
+
+			arrayLength.should.equal(array.length);
+		})
+	});
 });
