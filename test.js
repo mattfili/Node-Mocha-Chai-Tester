2f6cd4de7962b530bc3606f1ab880c299bcd75e2
Matt Fili <matthewjosephfili@gmail.com>
2015-07-21 09:57:02 -0500

    Added chai testing

--- test.js
+++ test.js
@@ -1,10 +1,9 @@
-var assert = require("assert")
+var should = require("chai").should()
 
-describe('Array', function() {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
+describe('Tests', function() {
+    it('Truthiness', function () {
+      true.should.equal(true);
+
+
     });
-  });
 });
